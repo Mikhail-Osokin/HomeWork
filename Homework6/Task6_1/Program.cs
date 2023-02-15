b821c69a359ee797db3be1b02bac732eadf6534c
Mikhail <un.less.jk@gmail.com>
2023-02-16 01:37:48 +0300

    Homework6

---
+++ Homework6/Task6_1/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+            Console.WriteLine("Ввели некорректное число. Повторите ввод");
+    }
+    return result;
+}
+
+int [] InitArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = GetNumber($"Введите {i+1}-е число: ");
+    }
+    return result;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
+
+void CountPositive(int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>0) count++;
+    }
+Console.WriteLine($"количество положительных чисел = {count}");
+}
+
+int lenght = GetNumber("Введите количество чисел: ");
+int [] arr = InitArray(lenght);
+PrintArray(arr);
+CountPositive(arr);
---
+++ Homework6/Task6_2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+            Console.WriteLine("Ввели некорректное число. Повторите ввод");
+    }
+    return result;
+}
+
+void GetCoordSolution(int b1, int b2, int k1, int k2)
+{
+    int x = 0;
+    int y = 0;
+    x = b1-b2/k2-k1;
+    y = k1*x+b1;
+    Console.WriteLine($"Точкой пересечения прямых y={k1}*x+{b1} и y={k2}*x+{b2} является ({x},{y})");
+}
+
+int k1 = GetNumber("Введите коэфициент k1 ");
+int b1 = GetNumber("Введите константу b1 ");
+int k2 = GetNumber("Введите коэфициент k2 ");
+int b2 = GetNumber("Введите константу b1 ");
+
+GetCoordSolution(b1,b2,k1,k2);
