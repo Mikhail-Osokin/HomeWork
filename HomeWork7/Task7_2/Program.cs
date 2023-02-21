e7f5393b77dd74c7b1e46fa7961419d9a44892e4
Mikhail <un.less.jk@gmail.com>
2023-02-21 04:22:26 +0300

    made all tasks HomeWork7

---
+++ HomeWork7/Task7_2/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+int GetNumber()
+{
+    int result;
+    while (true)
+    {
+        Console.WriteLine("Введите позицию в двумерном массиве ");
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число");
+        }
+    }
+    return result;
+}
+
+int [,] InitMatrix()
+{
+    int [,] matrix = new int [3,4];
+    Random rmd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rmd.Next(-9,10);
+        }
+    }
+return matrix;
+}
+
+void PrintMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void GetElementMatrFromPosition(int [,] matrix, int number)
+{
+    if (number > matrix.GetLength(0)*matrix.GetLength(1))
+    {
+        Console.WriteLine("Числа с такой позицией в заданном массиве нет");
+    }
+    else if (number%matrix.GetLength(1)==0)
+    {
+       Console.WriteLine($"Элемент двумерного массива с позцией {number} = {matrix[number/matrix.GetLength(1)-1,matrix.GetLength(1)-1]}"); 
+    }
+    else 
+    {
+        Console.WriteLine($"Элемент двумерного массива с позцией {number} = {matrix[number/matrix.GetLength(1),number%matrix.GetLength(1)-1]}");
+    }
+}
+
+int position = GetNumber();
+int [,] matrix = InitMatrix();
+PrintMatrix(matrix);
+GetElementMatrFromPosition(matrix,position);
---
+++ HomeWork7/Task7_3/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+*/
+
+int [,] InitMatrix()
+{
+    int [,] matrix = new int [3,4];
+    Random rmd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rmd.Next(-9,10);
+        }
+    }
+return matrix;
+}
+
+void PrintMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void PrintArray(double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double [] AverageRoWMatrix(int [,] matrix)
+{
+    double [] average = new double [matrix.GetLength(1)];
+    average[0] = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            average[i] = average[i] + matrix[j,i];
+        }
+    average[i] = average[i]/matrix.GetLength(0);
+    }
+return average;
+}
+
+int [,] matrix = InitMatrix();
+PrintMatrix(matrix);
+double [] average = AverageRoWMatrix(matrix);
+Console.WriteLine();
+PrintArray(average);
